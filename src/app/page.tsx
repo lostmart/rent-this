cddb37323787460a35d0214de5b0d2b7e1d0c26a
lostmart <martinpedraza1979@gmail.com>
2024-06-02 11:58:22 +0200

    navbar components and Image next comps

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@ import Link from "next/link"
 
 const Home = () => {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+		<div className="flex min-h-screen flex-col items-center justify-between p-24">
 			<h2>homa pahe</h2>
 			<Link href="addPage">Add page</Link>
-		</main>
+		</div>
 	)
 }
 
