eba91580445f9f89a387009a062bf12a268330a2
lostmart <martinpedraza1979@gmail.com>
2024-06-03 14:35:11 +0200

    new stymes and dynamic cards

--- src/app/carsPage/page.tsx
+++ src/app/carsPage/page.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import CarCard from "@/components/CarCard"
 import cars from "@/properties.json"
 
 type Tcar = {
@@ -21,7 +22,9 @@ const carsPage = () => {
 		<section className="px-4 py-6">
 			<div className="container-xl lg:container m-auto px-4 py-6">
 				<div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-					{cars.length ? cars.map((car: Tcar) => <div>{car.name} </div>): "No cars found ..."}
+					{cars.length ? cars.map((car: Tcar) => (
+						<CarCard key={car._id}>{car.name} </CarCard>
+					)) : "No cars found ..."}
 				</div>
 			</div>
 		</section>
