7c558988cee9dd81d774ff4b6dc69921eb87fc8d
lostmart <martinpedraza1979@gmail.com>
2024-06-07 17:02:58 +0200

    new env variables

--- src/utils/fetchCars.ts
+++ src/utils/fetchCars.ts
@@ -1,14 +1,20 @@
 import CarsType from "@/types/CarsType"
 
-export async function fetchCars(): Promise<CarsType | string> {
+const apiDomain = process.env.NEXT_API_DOMAIN || null
+
+export async function fetchCars(): Promise<CarsType | []> {
 	try {
-		const res = await fetch(`${process.env.NEXT_API_DOMAIN}/cars`)
+		// handle no domain available
+		if (!apiDomain) {
+			return []
+		}
+		const res = await fetch(`${apiDomain}/cars`)
 		if (!res.ok) {
 			throw new Error("Failed to fetch data ...")
 		}
 		return res.json()
 	} catch (error) {
 		console.log(error)
-		return "A terriblie error coccured bringing the data !"
+		return []
 	}
 }
